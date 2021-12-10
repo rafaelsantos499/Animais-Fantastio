493dbd917d64872242644ad1f54c5255b5c445cd
rafaelsantos499 <rafaelsantos499@gmail.com>
2021-12-09 21:58:40 -0300

    funcionamento refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,17 +1,38 @@
-export default function initFuncionamento() {
-    const funcionamento = document.querySelector('[data-semana]')
-    const diasSemana = funcionamento.dataset.semana.split(',').map(Number) // puxar o que esta dentro do dataset / split trasformando ne um array depois da vírgula
-    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number)
+export default class Funcionamento {
+    constructor(funcionamento, activeClass) {
+        this.funcionamento = document.querySelector(funcionamento)
+        this.activeClass = activeClass
+    }
 
-    const dataAgora = new Date();
-    const diaAgora = dataAgora.getDay(); // puxar o dia
-    const horarioAgora = dataAgora.getHours();
+    dadosFuncionamentos() {
+        // puxar o que esta dentro do dataset / split trasformando ne um array depois da vírgula
+        this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number)
+        this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number)
+    }
 
-    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1
+    dadosAgora() {
+        this.dataAgora = new Date();
+        this.diaAgora = this.dataAgora.getDay(); // puxar o dia
+        this.horarioAgora = this.dataAgora.getHours();
+    }
 
-    const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1])
-    // se os dois for true
-    if (semanaAberto && horarioAberto) {
-        funcionamento.classList.add('aberto')
+    estaAberto() {
+        const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1
+        const horarioAberto = (this.horarioAgora >= this.horarioSemana[0]
+            && this.horarioAgora < this.horarioSemana[1])
+
+        return semanaAberto && horarioAberto
+    }
+
+    ativaAberto() {
+        if (this.estaAberto()) this.funcionamento.classList.add(this.activeClass)
+    }
+
+    init() {
+        if (this.funcionamento) {
+            this.dadosFuncionamentos()
+            this.dadosAgora()
+            this.ativaAberto()
+        }
     }
 }
